f53ac0e0fb57ed1feba99fd88b1a3214db826a9a
coachi07 <ayenistephen07@gmail.com>
2024-10-30 10:33:47 +0100

    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

---
+++ area.py
@@ -0,0 +1,13 @@
+#write a phyton function code that will generate the area &perimeter of a rectangle 
+#if the length is 24 and the width is 16m
+#add perimiter and area togehther
+def area(length, breadth):
+    a=length*breadth
+    print(a)
+    p=(length+breadth)*2
+    print(p)
+    add=a+p
+    print(add)
+area(24,16)
+
+
